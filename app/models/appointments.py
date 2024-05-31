354ded2088be6a712629d1318e1faf608e716b33
doctormatterhorn <doctormatterhorn@gmail.com>
2024-05-31 13:47:11 -0300

    moved migration and added appointments file and object

---
+++ app/models/appointments.py
@@ -0,0 +1,18 @@
+from app import db
+from sqlalchemy import ForeignKey
+
+
+class Appointment(db.Model):
+    __tablename__ = 'appointments'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_crm = db.Column(db.Integer, ForeignKey('users.crm'), nullable=False)
+    center = db.Column(db.Text, nullable=False)
+    year = db.Column(db.Text, nullable=False)
+    month = db.Column(db.Text, nullable=False)
+    month_day = db.Column(db.Integer, nullable=False)
+    week_day = db.Column(db.Text, nullable=False)
+    starting_time = db.Column(db.Integer, nullable=False)
+    ending_time = db.Column(db.Integer, nullable=False)
+    is_holiday = db.Column(db.Boolean, default=False)
+    is_locked = db.Column(db.Boolean, default=False)
---
+++ migration.py
@@ -0,0 +1,9 @@
+from app import create_app, db
+
+
+app = create_app()
+
+with app.app_context():
+    db.create_all()
+    db.session.commit()
+    print('Database created!')
