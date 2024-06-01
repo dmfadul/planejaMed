0f4eb49cded5ea3656a230ba80d0b74ecb0e5754
doctormatterhorn <doctormatterhorn@gmail.com>
2024-06-01 01:00:43 -0300

    started working on migration functions

--- migration.py
+++ migration.py
@@ -1,11 +1,17 @@
 from app import create_app, db
+import migration.funcs as funcs
 
 
-app = create_app()
+data = funcs.load_from_db('Users', 1)
+print(data)
 
-with app.app_context():
-    # db.create_all()
-    # db.session.commit()
-    # print('Database created!')
 
-    pass
+
+# app = create_app()
+
+# with app.app_context():
+#     # db.create_all()
+#     # db.session.commit()
+#     # print('Database created!')
+
+#     pass
