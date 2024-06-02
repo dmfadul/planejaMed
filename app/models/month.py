dd209921eb68540564a19f4a5ab2ccf90f3689c4
doctormatterhorn <doctormatterhorn@gmail.com>
2024-06-02 16:03:26 -0300

    added is_current and is_locked to month

--- app/models/month.py
+++ app/models/month.py
@@ -7,5 +7,7 @@ class Month(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     year = db.Column(db.Text, nullable=False)
     name = db.Column(db.Text, nullable=False)
+    is_current = db.Column(db.Boolean, default=False)
+    is_locked = db.Column(db.Boolean, default=False)
 
     appointments = db.relationship('Appointment', backref='month', lazy=True)
