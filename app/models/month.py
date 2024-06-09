f922958eb519007e162c1b198122497204a8a522
doctormatterhorn <doctormatterhorn@gmail.com>
2024-06-09 12:24:01 -0300

    consolidated changes to Month model

--- app/models/month.py
+++ app/models/month.py
@@ -1,19 +1,40 @@
 from app import db
+from datetime import datetime, timedelta
 
 
 class Month(db.Model):
     __tablename__ = 'months'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-
     number = db.Column(db.Integer, nullable=False)
     year = db.Column(db.Integer, nullable=False)
-
     is_current = db.Column(db.Boolean, default=False)
     is_locked = db.Column(db.Boolean, default=False)
 
     days = db.relationship('Day', back_populates='month', lazy=True)
 
     def __repr__(self):
-        return f"{self.number}/{self.year}"
-    
+        return f"{self.number}/{self.year}"  
+
+    @classmethod
+    def gen_new_month(cls, number, year):
+        month = cls(number=number, year=year)
+        db.session.add(month)
+        db.session.commit()
+        return month
+
+    @property
+    def holidays(self):
+        return []
+
+    # @property
+    # def previous_month(self):
+    #     self.number -= 1
+
+    # @property
+    # def next_month(self):
+    #     pass
+
+    @property
+    def dates_row():
+        return []
--- cli.py
+++ cli.py
@@ -1,5 +1,5 @@
 import migration_funcs
-from app.models import User, Center
+from app.models import User, Center, Month
 from app import create_app
 from app.models._funcs import unify_appointments
 
@@ -8,6 +8,7 @@ from app.models._funcs import unify_appointments
 # migration_funcs.migrate_users()
 # migration_funcs.migrate_base("CCQ--BASE")
 
-# app = create_app()
-# with app.app_context():
-#     print(User.get_crm("Nikolas Kim"))
+app = create_app()
+with app.app_context():
+    month = Month.gen_new_month(12, 2023)
+    print(month)
