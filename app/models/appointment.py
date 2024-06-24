e8513e3ccb45c240a90b5ae12832befb764772aa
doctormatterhorn <doctormatterhorn@gmail.com>
2024-06-23 22:17:14 -0300

    added relation between request and appointment

--- app/models/appointment.py
+++ app/models/appointment.py
@@ -10,15 +10,16 @@ class Appointment(db.Model):
     user_id = db.Column(db.Integer, ForeignKey('users.id'), nullable=False)
     center_id = db.Column(db.Integer, ForeignKey('centers.id'), nullable=False)
     day_id = db.Column(db.Integer, ForeignKey('days.id'), nullable=False)
+    request_id = db.Column(db.Integer, ForeignKey('requests.id'), nullable=True)
     hour = db.Column(db.Integer, nullable=False)
 
     user = db.relationship('User', back_populates='appointments', lazy=True)
     center = db.relationship('Center', back_populates='appointments', lazy=True)
     day = db.relationship('Day', back_populates='appointments', lazy=True)
-    
-    general_requests = db.relationship('Request', foreign_keys='Request.existing_appointment_id', back_populates='existing_appointment', lazy=True)
-    requests_to_exchange = db.relationship('Request', foreign_keys='Request.appointment_to_exchange_id', back_populates='appointment_to_exchange', lazy=True)
+    request = db.relationship('Request', back_populates='appointment', lazy=True)
 
+    is_confirmed = db.Column(db.Boolean, default=True)
+    
     __table_args__ = (UniqueConstraint('user_id', 'day_id', 'hour', name='unique_appointment'),)
     __table_args__ = (CheckConstraint('hour >= 0 AND hour < 24', name='valid_hour_range'),)
     
--- app/models/request.py
+++ app/models/request.py
@@ -12,23 +12,15 @@ class Request(db.Model):
     receivers_code = db.Column(db.Text, nullable=False)
     action = db.Column(db.Text, nullable=False)
 
-    is_open = db.Column(db.Boolean, default=True)
     creation_date = db.Column(db.Date, nullable=False, default=datetime.now())
-
-    existing_appointment_id = db.Column(db.Integer, ForeignKey('appointments.id'))
-    appointment_to_exchange_id = db.Column(db.Integer, ForeignKey('appointments.id'))
-    doctor_who_will_cover_id = db.Column(db.Integer, ForeignKey('users.id'))
-    doctor_to_include_id = db.Column(db.Integer, ForeignKey('users.id'))
-
-    authorized = db.Column(db.Boolean, nullable=True)
+    is_open = db.Column(db.Boolean, default=True)
     response_date = db.Column(db.Date, nullable=True)
+    response = db.Column(db.Text, nullable=True)
 
     requester = db.relationship('User', foreign_keys=[requester_id], back_populates='requests_sent', lazy=True)
     responder = db.relationship('User', foreign_keys=[responder_id], back_populates='requests_received', lazy=True)
-    existing_appointment = db.relationship('Appointment', foreign_keys=[existing_appointment_id], back_populates='general_requests', lazy=True)
-    appointment_to_exchange = db.relationship('Appointment', foreign_keys=[appointment_to_exchange_id], back_populates='requests_to_exchange', lazy=True)
-    doctor_who_will_cover = db.relationship('User', foreign_keys=[doctor_who_will_cover_id], lazy=True)
-    doctor_to_include = db.relationship('User', foreign_keys=[doctor_to_include_id], lazy=True)
+
+    appointments = db.relationship('Appointment', back_populates='request', lazy=True)
     
     def __repr__(self):
         return f'{self.requester} - {self.action} - {self.is_open}'
@@ -38,10 +30,8 @@ class Request(db.Model):
         new_request = cls(requester_id=doctor_to_include_id,
                           receivers_code="*",
                           action="include_user",
-                          existing_appointment_id=None,
-                          appointment_to_exchange_id=None,
-                          doctor_who_will_cover_id=None,
-                          doctor_to_include_id=doctor_to_include_id)
+                          existing_appointment_id=None
+                          )
         db.session.add(new_request)
         db.session.commit()
         return new_request
