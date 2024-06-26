b45c4fa56add2b3113399dde5af6f66c8238fe01
doctormatterhorn <doctormatterhorn@gmail.com>
2024-06-26 18:03:07 -0300

    fixed js funcs errors when user is not admin

--- app/static/scripts/buttonListeners.js
+++ app/static/scripts/buttonListeners.js
@@ -1,13 +1,3 @@
-document.getElementById("sum-hours-button").addEventListener("click", function() {
-    // Collect the data
-    var center = centerValue;
-    var month = monthValue;
-    var year = yearValue;
-
-    // Redirect to the new page with the data
-    window.location.href = `/sum-days/${center}/${month}/${year}`;
-});
-
 document.getElementById("print-table").addEventListener("click", function() {
     // Collect the data
     var center = centerValue;
@@ -18,38 +8,51 @@ document.getElementById("print-table").addEventListener("click", function() {
     window.location.href = `/print-table/${center}/${month}/${year}`;
 });
 
-document.getElementById("gen-report").addEventListener("click", function() {
-    // Collect the data
-    var center = centerValue;
-    var month = monthValue;
-    var year = yearValue;
+if (isAdmin) {
+    document.getElementById("sum-hours-button").addEventListener("click", function() {
+        // Collect the data
+        var center = centerValue;
+        var month = monthValue;
+        var year = yearValue;
 
-    // Redirect to the new page with the data
-    window.location.href = `/report/${center}/${month}/${year}`;
-});
+        // Redirect to the new page with the data
+        window.location.href = `/sum-days/${center}/${month}/${year}`;
+    });
 
-function handleCreateButtonClick() {
-    // Show confirmation dialog
-    const userConfirmed = confirm("Confirmar a criação de um Novo Mês. Tem a certeza que deseja continuar?");
-    
-    if (userConfirmed) {
-        // Show the waiting message
-        document.getElementById('waitingMessage').style.display = 'block';
-        
-        // Redirect to the URL (simulate form submission or AJAX request)
-        window.location.href = '/admin/create-month';
+
+    document.getElementById("gen-report").addEventListener("click", function() {
+        // Collect the data
+        var center = centerValue;
+        var month = monthValue;
+        var year = yearValue;
+
+        // Redirect to the new page with the data
+        window.location.href = `/report/${center}/${month}/${year}`;
+    });
+
+    function handleCreateButtonClick() {
+        // Show confirmation dialog
+        const userConfirmed = confirm("Confirmar a criação de um Novo Mês. Tem a certeza que deseja continuar?");
+
+        if (userConfirmed) {
+            // Show the waiting message
+            document.getElementById('waitingMessage').style.display = 'block';
+
+            // Redirect to the URL (simulate form submission or AJAX request)
+            window.location.href = '/admin/create-month';
+        }
     }
-}
 
-function handleNextButtonClick() {
-    // Show confirmation dialog
-    const userConfirmed = confirm("Confirmar a Liberação de um Novo Mês. Tem a certeza que deseja continuar?");
-    
-    if (userConfirmed) {
-        // Show the waiting message
-        document.getElementById('waitingMessage').style.display = 'block';
-        
-        // Redirect to the URL (simulate form submission or AJAX request)
-        window.location.href = '/admin/next-month';
+    function handleNextButtonClick() {
+        // Show confirmation dialog
+        const userConfirmed = confirm("Confirmar a Liberação de um Novo Mês. Tem a certeza que deseja continuar?");
+
+        if (userConfirmed) {
+            // Show the waiting message
+            document.getElementById('waitingMessage').style.display = 'block';
+
+            // Redirect to the URL (simulate form submission or AJAX request)
+            window.location.href = '/admin/next-month';
+        }
     }
 }
