615098e430f2dcc77e05e3df788cdf1455999425
doctormatterhorn <dmf030@gmail.com>
2024-11-09 23:30:27 -0300

    added payVacation()

--- app/routes/admin/static/scripts/showPay.js
+++ app/routes/admin/static/scripts/showPay.js
@@ -1,3 +1,23 @@
+function payVacation(button) {
+    const vacationID = button.getAttribute('data-id');
+    
+    fetch('/admin/pay-vacation', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ vacationID: vacationID }),
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Success:', data);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
+
+
 function showPay(button) {
     const vacationID = button.getAttribute('data-id');
     
