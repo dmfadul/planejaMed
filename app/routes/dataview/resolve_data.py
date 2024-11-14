fa17f8824feefa85c71a43141af854db149e895f
doctormatterhorn <dmf030@gmail.com>
2024-11-14 14:22:22 -0300

    added auto request to add apps

--- app/routes/dataview/resolve_data.py
+++ app/routes/dataview/resolve_data.py
@@ -59,6 +59,9 @@ def resolve_base_appointments(data):
 
 
 def resolve_month_appointments(data):
+    from app.models import Request
+
+    system_user = User.query.filter_by(crm=0).first()
     action = data.get("action")
     center = Center.query.filter_by(abbreviation=data.get("center")).first()
     year = int(data.get("year"))
@@ -86,9 +89,21 @@ def resolve_month_appointments(data):
                     is_confirmed=True
                 ).all()
 
+                appointments.delete_requests()
+
+                hours = [appointment.hour for appointment in appointments]
+                req = Request.exclusion(doctor=doctor,
+                                        center=center,
+                                        day=day,
+                                        hours=hours,
+                                        requester=system_user)
+
+                if not isinstance(req, str):
+                    req.close(system_user.id, "authorized")
+
                 for appointment in appointments:
-                    appointment.delete_entry()
-            
+                    appointment.delete_entry(del_requests=False)
+                   
             return 0
 
         elif action in ["add", "add-direct"]:
@@ -99,6 +114,15 @@ def resolve_month_appointments(data):
 
             app = create_app()
             with app.app_context():
+                req = Request.inclusion(doctor=doctor,
+                                        center=center,
+                                        day=day,
+                                        hours=hours,
+                                        requester=system_user)
+
+                if not isinstance(req, str):
+                    req.close(system_user.id, "authorized")
+
                 flags = []
                 for hour in hours:
                     flag = Appointment.add_entry(doctor.id, center.id, day.id, hour)
