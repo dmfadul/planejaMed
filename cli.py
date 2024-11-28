b967492a7850752631d0f493b90cb5de214e9d2d
doctormatterhorn <dmf030@gmail.com>
2024-11-27 23:15:58 -0300

    added temp func to create list of dates

--- cli.py
+++ cli.py
@@ -13,33 +13,81 @@ import json
 
 app = create_app()
 with app.app_context():
-    user = User.query.filter_by(crm=42217).first()
-    print(user.full_name)
-    v = Month.check_vacation_entitlement(user.id, 6, 2024)
-    print(v)
+    # user = User.query.filter_by(crm=42217).first()
+    # print(user.full_name)
+    # v = Month.check_vacation_entitlement(user.id, 6, 2024)
+    # print(v)
 
-    # user_dates = ""
-    # users = sorted(User.query.all(), key=lambda x: x.full_name)
-    # for user in users:
-    #     if not user.is_active or not user.is_visible:
-    #         continue
-      
-    #     results = {}
-    #     for m in range(1, 13):
-    #         v = Month.check_vacation_entitlement(user.id, m, 2024)
-    #         if isinstance(v, str):
-    #             if 'rotina' in v:
-    #                 results[m] = 'ROT'
-    #             elif 'plantão' in v:
-    #                 results[m] = 'PLT'
-    #             elif 'Base' in v:
-    #                 continue
-    #             else:
-    #                 results[m] = v
-    #         else:
-    #             results[m] = v
+    user_dates = ""
+    done_users = []
+    user_dates += "ADMINITRATIVO\n\n"
+    
+    users = sorted(User.query.all(), key=lambda x: x.full_name)
+    for user in users:
+        if not user.is_active or not user.is_visible:
+            done_users.append(user.id)
+            continue
+        
+        if user.pre_approved_vacation:
+            user_dates += f"{user.abbreviated_name}\n"
+            done_users.append(user.id)
 
-    #     print(f"{user.abbreviated_name} - {results}")
+    
+    user_dates += "\n\nSEM DIREITO BASE\n\n"
+    for user in users:
+        if user.id in done_users:
+            continue
+
+        base_test = Month.check_vacation_entitlement(user.id, 11, 2024)
+        if isinstance(base_test, str) and "Base" in base_test:
+            user_dates += f"{user.abbreviated_name}\n"
+            done_users.append(user.id)
+
+    
+    user_dates += "\n\n CUMPREM AS REGRAS HÁ MAIS DE SEIS MESES\n\n"
+    for user in users:
+        if user.id in done_users:
+            continue
+
+        break_flag = False
+        for m in range(12, 5, -1):
+            v = Month.check_vacation_entitlement(user.id, m, 2024)
+
+            if isinstance(v, str):
+                if ('rotina' in v) or ('plantão' in v):
+                    break_flag = True
+                    break
+        
+        if break_flag:
+            continue
+        
+        done_users.append(user.id)
+        user_dates += f"{user.abbreviated_name}\n"
+
+    
+    user_dates += "\n\nOUTROS\n\n"
+    for user in users:
+        if user.id in done_users:
+            continue
+
+        break_flag = False
+        for m in range(12, 5, -1):
+            v = Month.check_vacation_entitlement(user.id, m, 2024)
+
+            if isinstance(v, str):
+                if ('rotina' in v) or ('plantão' in v):
+                    break_flag = True
+                    date = datetime(2024, m, 26)
+                    user_dates += f"{user.abbreviated_name} \t\t-\t\t {date.date().strftime("%d/%m/%Y")}\n"
+                    break
+        
+        if break_flag:
+            continue
+        
+        user_dates += f"{user} - {user.date_joined}\n"
+        
+    with open("user_dates.txt", "w") as f:
+        f.write(user_dates)
 
 
         # if all([v == 0 for v in results.values()]):
