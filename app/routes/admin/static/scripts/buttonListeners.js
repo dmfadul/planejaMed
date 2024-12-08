f2c01884338b062fa6b945d189292d0fc4951e6b
doctormatterhorn <dmf030@gmail.com>
2024-12-08 10:56:22 -0300

    added getVacationRights on buttonListeneres.js

--- app/routes/admin/static/scripts/buttonListeners.js
+++ app/routes/admin/static/scripts/buttonListeners.js
@@ -20,12 +20,24 @@ function getVacationRights() {
         if (this.status === 200) {
             let vacationRights = JSON.parse(this.responseText);
             console.log(vacationRights);
+
+            const modal = document.getElementById('Modal');
+            const overlay = document.getElementById('overlay');
+
+            // Set the modal position based on the current scroll position
+            modal.style.top = `${window.scrollY + window.innerHeight / 2 - modal.offsetHeight / 2}px`;
+            modal.style.left = `${window.innerWidth / 2 - modal.offsetWidth / 2}px`;
+
+            modal.classList.remove('hidden');
+            overlay.classList.remove('hidden');
+
         } else {
             console.log("Error: Could not get Vacations Rights");
         }
     }
     xhr.send();
 }
+
 
 function handleNextButtonClick() {
     // Show confirmation dialog
