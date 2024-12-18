f61a6a2571c923ed71e89af73366c036f5d78f76
doctormatterhorn <dmf030@gmail.com>
2024-12-17 22:15:39 -0300

    added change-privilege-status function

--- app/routes/admin/static/scripts/showPay.js
+++ app/routes/admin/static/scripts/showPay.js
@@ -1,3 +1,32 @@
+function changeStatus(button, newStatus) {
+    const vacationID = button.getAttribute('data-id');
+
+    // Add a confirmation dialog
+    if(newStatus === 'deleted') {
+        const userConfirmed = confirm('Tem a certeza que deseja apagar este pedido? Esta ação é irreversível.');
+        if (!userConfirmed) {
+            return; // Stop execution if the user cancels
+        }
+    }
+
+    fetch('/admin/change-privilege-status', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ vacationID: vacationID, newStatus: newStatus }),
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Success:', data);
+        location.reload();
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
+
+
 function payVacation(button) {
     const vacationID = button.getAttribute('data-id');
     
