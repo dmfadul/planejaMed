ce0f64407d8593a0fa541c2d08fca9ef7c95eff0
dmfadul <dmf030@gmail.com>
2025-01-09 15:16:16 -0300

    moved mainModal creation to vacations.js

--- app/static/scripts/vacations.js
+++ app/static/scripts/vacations.js
@@ -7,12 +7,43 @@ function getPrivilegeRights() {
             let vacationRights = JSON.parse(this.responseText);
 
             const modal = document.getElementById('mainModal');
+            const modalContent = document.getElementById('mainModalContent');
             const overlay = document.getElementById('mainoverlay');
+
+            const modalTitle = document.createElement('h2');
+            modalTitle.innerHTML = 'Checagem Férias';
+
+            const modalBody = document.createElement('div');
+            modalBody.id = 'mainModalBody';
+            modalBody.innerHTML = vacationRights;
+
+            const buttonGroup = document.createElement('div');
+            buttonGroup.classList.add('button-group');
+
+            const closeButton = document.createElement('button');
+            closeButton.textContent = 'Fechar';
+            closeButton.classList.add('submit-button', 'cancel-button');
+            closeButton.onclick = () => {
+                closeMainModal();
+            };
+
+            const actionButton = document.createElement('button');
+            actionButton.textContent = 'Mandar Mensagens';
+            actionButton.classList.add('submit-button', 'save-button', 'disabled-button');
+            actionButton.onclick = () => {
+            };
+
+            buttonGroup.appendChild(closeButton);
+            buttonGroup.appendChild(actionButton);
+
+            modalContent.appendChild(modalTitle);
+            modalContent.appendChild(modalBody);
+            modalContent.appendChild(buttonGroup);
 
             // Set the modal position based on the current scroll position
             modal.style.top = `${window.scrollY + window.innerHeight / 2 - modal.offsetHeight / 2}px`;
             modal.style.left = `${window.innerWidth / 2 - modal.offsetWidth / 2}px`;
-            document.getElementById('mainModalBody').innerHTML = vacationRights;
+            // document.getElementById('mainModalBody').innerHTML = vacationRights;
 
             modal.classList.remove('hidden');
             overlay.classList.remove('hidden');
